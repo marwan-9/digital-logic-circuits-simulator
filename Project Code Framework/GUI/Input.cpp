cb3299d015a3df5b7b030bbe78aa150181a75843
YoussofEldeeb <76092209+YoussofEldeeb@users.noreply.github.com>
2020-12-16 14:19:49 +0200

    Update Input.cpp

--- Project Code Framework/GUI/Input.cpp
+++ Project Code Framework/GUI/Input.cpp
@@ -1,71 +1,192 @@
-//#include "Input.h"
+#include "Input.h"
 #include "Output.h"
 
-Input::Input(window* pW)
-{
-	pWind = pW; //point to the passed window
+
+Input::Input(window* pW) {
+	pWind = pW;
 }
 
-void Input::GetPointClicked(int &x, int &y)
-{
-	pWind->WaitMouseClick(x, y);	//Wait for mouse click
+// last point clicked
+void Input::GetPointClicked(int& x, int& y) const {
+	x = LastX;
+	y = LastY;
 }
 
-string Input::GetSrting(Output *pOut)
-{
-	///TODO: Implement this Function
-	//Read a complete string from the user until the user presses "ENTER".
-	//If the user presses "ESCAPE". This function should return an empty string.
-	//"BACKSPACE" should be also supported
-	//User should see what he is typing at the status bar
-
-	return NULL;
+// mouse click coordinate
+void Input::WaitMouseClick(int& x, int& y) {
+	pWind->WaitMouseClick(x, y);
+	LastX = x;
+	LastY = y;
 }
 
-//This function reads the position where the user clicks to determine the desired action
-ActionType Input::GetUserAction() const
-{	
-	int x,y;
-	pWind->WaitMouseClick(x, y);	//Get the coordinates of the user click
+// current state of the mouse buttons and position
+buttonstate Input::GetButtonState(const button Mouse, int& x, int& y) {
+	return pWind->GetButtonState(Mouse, x, y);;
+}
 
-	if(UI.AppMode == DESIGN )	//application is in design mode
-	{
-		//[1] If user clicks on the Toolbar
-		if ( y >= 0 && y < UI.ToolBarHeight)
-		{	
-			//Check whick Menu item was clicked
-			//==> This assumes that menu items are lined up horizontally <==
-			int ClickedItemOrder = (x / UI.ToolItemWidth);
-			//Divide x coord of the point clicked by the menu item width (int division)
-			//if division result is 0 ==> first item is clicked, if 1 ==> 2nd item and so on
+// Returns user key press
+keytype Input::GetKeyPress(char& key) {
 
-			switch (ClickedItemOrder)
-			{
-			case ITM_AND2: return ADD_AND_GATE_2;
-			case ITM_OR2: return ADD_OR_GATE_2;
-			case ITM_EXIT: return EXIT;	
-			
-			default: return DSN_TOOL;	//A click on empty place in desgin toolbar
+	return pWind->GetKeyPress(key);
+}
+
+// Returns the user's key press without removing it from the queue
+keytype Input::GetKeyState(char& key) {
+	return pWind->GetKeyState(key);
+}
+
+// Returns the string entered by the user and reflect it on the status bar
+string Input::GetSrting(Output* pOut, string msg, string str) const {
+	// ASCII Codes
+	const int ESCAPE = 27;
+	const int ENTER = 13;
+	const int BACKSPACE = 8;
+
+	char c;
+	string s = str;
+
+	pWind->FlushKeyQueue();
+
+	do {
+		pOut->PrintMsg(msg + " " + s);
+		pWind->WaitKeyPress(c);
+		
+		switch (c) {
+		case BACKSPACE:
+			if (!s.empty()) s.pop_back();
+			break;
+		case ESCAPE:
+			s.clear();
+			break;
+		case ENTER:
+			// Nothing
+			break;
+		default:
+			s.push_back(c);
+			break;
+		}
+
+	} while (c != ESCAPE && c != ENTER);
+
+	return s;
+}
+
+// Reads the user's selection and determine the desired action 
+ActionType Input::GetUserAction(Output* pOut) {
+	pWind->FlushMouseQueue();
+
+	int x, y;
+	char hotKey;
+	keytype keyType;
+
+	while (GetButtonState(LEFT_BUTTON, x, y) == BUTTON_UP) {
+		keyType = pWind->GetKeyState(hotKey);
+
+		if (keyType == NO_KEYPRESS) {
+			return ActionType::HOVER;
+		}
+
+		// Detecting actions using hotkeys
+		if (UI.AppMode == MODE::DESIGN) {
+			if (keyType == keytype::KEY_DEL) {
+				pWind->FlushKeyQueue();
+				return ActionType::DEL;
+			}
+			else if (keyType == ASCII) {
+				switch (hotKey)
+				{
+				case CTRL_A: return ActionType::SELECT;
+				case CTRL_S: pWind->FlushKeyQueue(); return ActionType::SAVE;
+				case CTRL_C: pWind->FlushKeyQueue(); return ActionType::COPY;
+				case CTRL_X: pWind->FlushKeyQueue(); return ActionType::CUT;
+				case CTRL_V: pWind->FlushKeyQueue(); return ActionType::PASTE;
+				case CTRL_Z: pWind->FlushKeyQueue(); return ActionType::UNDO;
+				case CTRL_Y: pWind->FlushKeyQueue(); return ActionType::REDO;
+				case CTRL_O: pWind->FlushKeyQueue(); return ActionType::LOAD;
+				}
 			}
 		}
-	
-		//[2] User clicks on the drawing area
-		if ( y >= UI.ToolBarHeight && y < UI.height - UI.StatusBarHeight)
-		{
-			return SELECT;	//user want to select/unselect a component
-		}
 		
-		//[3] User clicks on the status bar
-		return STATUS_BAR;
-	}
-	else	//Application is in Simulation mode
-	{
-		return SIM_MODE;	//This should be changed after creating the compelete simulation bar 
+		pWind->FlushKeyQueue();
 	}
 
+	LastX = x;
+	LastY = y;
+
+	// User clicks on the drawing area
+	// + GateBarHeight (optional)
+	if (y >= UI.ToolBarHeight && y < UI.height - UI.StatusBarHeight) {
+		if (UI.AppMode == MODE::DESIGN) {
+			///////////////////////////////////////////To-Do ADD GATES/////////////////////////////////////////////////////////////////////////////////////
+		}}
+
+	pWind->WaitMouseClick(x, y);
+
+	// User clicks on the tool bar
+	if (y >= 0 && y < UI.ToolBarHeight) {
+		int clickedItemOrder = x / UI.ToolItemWidth;
+
+		if (UI.AppMode == MODE::DESIGN) {
+			switch (clickedItemOrder) {
+			//  case DsgnMenuItem::ITM_EDIT: return ActionType::EDIT;
+			case DsgnMenuItem::ITM_DEL: return ActionType::DEL;
+			case DsgnMenuItem::ITM_COPY: return ActionType::COPY;
+			case DsgnMenuItem::ITM_CUT: return ActionType::CUT;
+			case DsgnMenuItem::ITM_PASTE: return ActionType::PASTE;
+			case DsgnMenuItem::ITM_UNDO: return ActionType::UNDO;
+			case DsgnMenuItem::ITM_REDO: return ActionType::REDO;
+			//  case DsgnMenuItem::ITM_PLAY: return ActionType::SIMULATION_MODE;
+			case DsgnMenuItem::ITM_SAVE: return ActionType::SAVE;
+			case DsgnMenuItem::ITM_LOAD: return ActionType::LOAD;
+			case DsgnMenuItem::ITM_EXIT: return ActionType::EXIT;
+			default: return ActionType::STATUS_BAR; //tool_bar
+			}
+		}
+		else {
+			switch (clickedItemOrder) {
+			//case DsgnMenuItem::ITM_PAUSE: return ActionType::DESIGN_MODE;
+			case SimMenuItem::ITM_TRUTH: return ActionType::Create_TruthTable;
+			case DsgnMenuItem::ITM_EXIT:	return ActionType::EXIT;
+			default: return ActionType::STATUS_BAR; //tool_bar
+			}
+		}
+	}
+	// User clicks on gate bar
+	else if (y >= UI.ToolBarHeight && y < UI.ToolBarHeight + UI.StatusBarHeight) {
+		int clickedItemOrder = x / UI.MenuItemWidth;
+
+		if (UI.AppMode == MODE::DESIGN) {
+			switch (clickedItemOrder) {
+			case DsgnMenuItem::ITM_SWITCH: return ActionType::ADD_Switch;
+			case DsgnMenuItem::ITM_AND2: return ActionType::ADD_AND_GATE_2;
+			case DsgnMenuItem::ITM_NAND2: return ActionType::ADD_NAND_GATE_2;
+			case DsgnMenuItem::ITM_AND3: return ActionType::ADD_AND_GATE_3;
+			case DsgnMenuItem::ITM_BUFF: return ActionType::ADD_Buff;
+			case DsgnMenuItem::ITM_INV: return ActionType::ADD_INV;
+			case DsgnMenuItem::ITM_OR2: return ActionType::ADD_OR_GATE_2;
+			case DsgnMenuItem::ITM_NOR2: return ActionType::ADD_NOR_GATE_2;
+			case DsgnMenuItem::ITM_NOR3: return ActionType::ADD_NOR_GATE_3;
+			case DsgnMenuItem::ITM_XOR2: return ActionType::ADD_XOR_GATE_2;
+			case DsgnMenuItem::ITM_XNOR2: return ActionType::ADD_XNOR_GATE_2;
+			case DsgnMenuItem::ITM_XOR3: return ActionType::ADD_XOR_GATE_3;
+			case DsgnMenuItem::ITM_WIRE: return ActionType::ADD_CONNECTION;
+			case DsgnMenuItem::ITM_LED: return ActionType::ADD_LED;
+			//default:return ActionType::GATE_BAR;
+			}
+		}
+		else {
+			//Check if there will be a gate bar
+			//return ActionType::GATE_BAR;
+		}
+	}
+	// User clicks on status bar
+	else {
+		return ActionType::STATUS_BAR;
+	}		
 }
 
 
-Input::~Input()
-{
+Input::~Input() {
+
 }
+	
