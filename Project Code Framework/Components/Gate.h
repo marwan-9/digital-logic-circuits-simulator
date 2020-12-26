4fd0f233aab74ad7350a6a5452182a965bca7d2a
YoussofEldeeb <76092209+YoussofEldeeb@users.noreply.github.com>
2020-12-26 17:29:23 +0200

    Update Gate.h

--- Project Code Framework/Components/Gate.h
+++ Project Code Framework/Components/Gate.h
@@ -13,16 +13,39 @@
 #include "OutputPin.h"
 #include "Component.h"
 
-class Gate:public Component
+#pragma once
+#include "Component.h"
+#include "inputpin.h"
+#include "outputpin.h"
+ 
+ 
+class Gate : public Component
 {
 protected:
 	InputPin* m_InputPins;	//Array of input pins of the Gate
 	OutputPin m_OutputPin;	//The Gate output pin
 	int m_Inputs;		//No. of input pins of that Gate.
 public:
+ 
+	//Gate(Output* pOut, const GraphicsInfo& gfxInfo);
 	Gate(int r_Inputs, int r_FanOut);
-	
-
+ 
+	void SetGraphicsInfo(Output* pOut, const GraphicsInfo& gfxInfo);
+ 
+	virtual int GetInputPinIndex(int x, int y);
+ 
+	virtual void GetInputPinCoordinates(int& x, int& y, int n);
+ 
+	virtual void GetOutputPinCoordinates(int& x, int& y);
+ 
+	virtual Pin* GetInputPin(int n) = 0;
+ 
+	virtual Pin* GetOutputPin() = 0;
+ 
+	virtual Component* GetNextComponent(int index) = 0;
+ 
+	virtual int GetConnectedCount() = 0;
+ 
+	virtual ~Gate();
 };
-
 #endif
