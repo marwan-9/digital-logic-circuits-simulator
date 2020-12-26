a6c988683c6395611682dc0da1d111b8b3cfa2a9
YoussofEldeeb <76092209+YoussofEldeeb@users.noreply.github.com>
2020-12-26 17:24:47 +0200

    Update Gate.cpp

--- Project Code Framework/Components/Gate.cpp
+++ Project Code Framework/Components/Gate.cpp
@@ -1,16 +1,36 @@
 #include "Gate.h"
-
-//Gate Constructor
-//Parameters:
-//r_Inputs: no. of gate's input pins
-//r_FanOut: Fan out of the gate's output pin
-Gate::Gate(int r_Inputs, int r_FanOut):m_OutputPin(r_FanOut)
-{
-	//Allocate number of input pins (equals r_Inputs)
-	m_InputPins = new InputPin[r_Inputs];
-	m_Inputs = r_Inputs;	//set no. of inputs of that gate
-	
-	//Associate all input pins to this gate
-	for(int i=0; i<m_Inputs; i++)
-		m_InputPins[i].setComponent(this);
+ 
+ 
+Gate::Gate(Output* pOut, const GraphicsInfo& gfxInfo) {
+	SetGraphicsInfo(pOut, gfxInfo);
+}
+ 
+ 
+void Gate::SetGraphicsInfo(Output* pOut, const GraphicsInfo& gfxInfo) {
+ 
+ 
+	mGfxInfo = gfxInfo;
+	pOut->MarkPinArea(mGfxInfo, PinType::GATE, this); //an Output function
+}
+ 
+ 
+int Gate::GetInputPinIndex(int x, int y) {
+	return 0;
+}
+ 
+ 
+void Gate::GetInputPinCoordinates(int& x, int& y, int n) {
+	x = mGfxInfo.x1 - UI.Margin;
+	y = mGfxInfo.y1 + UI.Margin + UI.Offset * n;
+}
+ 
+ 
+void Gate::GetOutputPinCoordinates(int& x, int& y) {
+	x = mGfxInfo.x2 + UI.Margin;
+	y = mGfxInfo.y1 + (mGfxInfo.y2 - mGfxInfo.y1) / 2;
+}
+ 
+ 
+Gate::~Gate() {
+ 
 }
