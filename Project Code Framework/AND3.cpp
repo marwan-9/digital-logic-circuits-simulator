c9017d4cca36e0ac5658c4bfa47f5aceaa7bb87a
ahmed1234552 <74154389+ahmed1234552@users.noreply.github.com>
2020-12-31 21:27:10 +0200

    Update Project Code Framework/AND3.cpp
    Co-authored-by: Khalidmamdou7 <37771185+Khalidmamdou7@users.noreply.github.com>

--- Project Code Framework/AND3.cpp
+++ Project Code Framework/AND3.cpp
@@ -19,7 +19,7 @@ void AND3::Operate()
 void AND3::Draw(Output* pOut)
 {
 	//Call output class and pass gate drawing info to it.
-	pOut->DrawNOR3(m_GfxInfo);
+	pOut->DrawAND3(m_GfxInfo);
 }
 
 //returns status of outputpin
