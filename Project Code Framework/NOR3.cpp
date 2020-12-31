2bb5f1c713f84bd208640224fcddeec1c5ab7c7a
ahmed1234552 <74154389+ahmed1234552@users.noreply.github.com>
2020-12-31 22:01:40 +0200

    Update Project Code Framework/NOR3.cpp
    Co-authored-by: Khalidmamdou7 <37771185+Khalidmamdou7@users.noreply.github.com>

--- Project Code Framework/NOR3.cpp
+++ Project Code Framework/NOR3.cpp
@@ -1,6 +1,6 @@
 #include "NOR3.h"
 
-NOR3::NOR3(const GraphicsInfo& r_GfxInfo, int r_FanOut) :Gate(2, r_FanOut)
+NOR3::NOR3(const GraphicsInfo& r_GfxInfo, int r_FanOut) :Gate(3, r_FanOut)
 {
 	m_GfxInfo = r_GfxInfo;
 }
