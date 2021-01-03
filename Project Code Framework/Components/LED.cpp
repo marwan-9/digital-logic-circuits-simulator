7983b96fae9413bd431f360b56418d0ff7ccafad
YoussofEldeeb <76092209+YoussofEldeeb@users.noreply.github.com>
2021-01-03 14:43:39 +0200

    Add files via upload

---
+++ Project Code Framework/Components/LED.cpp
@@ -0,0 +1,35 @@
+#include "LED.h"
+
+/* Constructor */
+LED::LED(const GraphicsInfo &r_GfxInfo, int r_FanOut):Gate(2, r_FanOut)
+{
+	m_GfxInfo.x1 = r_GfxInfo.x1;
+	m_GfxInfo.y1 = r_GfxInfo.y1;
+	m_GfxInfo.x2 = r_GfxInfo.x2;
+	m_GfxInfo.y2 = r_GfxInfo.y2;
+}
+
+/* Sets the status of the input pin number n (0-indexed) */
+void LED::setInputPinStatus(int n, STATUS s) {
+	m_InputPins[n-1].setStatus(s);
+}
+
+/* Returns the status of the input pin number n (0-indexed) */
+int LED::GetInputPinStatus(int n) {
+	return m_InputPins[n-1].getStatus();
+}
+
+/* Returns the status of the output pin */
+int LED::GetOutPinStatus() {
+	return -1;
+}
+
+/* Calculates the output of the LED */
+void LED::Operate() {
+	return;
+}
+
+/* Draws the LED */
+void LED::Draw(Output* pOut) {
+	pOut->DrawLED(m_GfxInfo);
+}
---
+++ Project Code Framework/Components/LED.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "../Components/Gate.h"
+
+class LED : 
+
+	public Gate {
+public:
+
+	LED(const GraphicsInfo &r_GfxInfo, int r_FanOut);
+	virtual void Operate();	//Calculates the output of the AND gate
+	virtual void Draw(Output* pOut);	//Draws 2-input gate
+
+	virtual int GetOutPinStatus();	//returns status of outputpin if LED, return -1
+	virtual int GetInputPinStatus(int n);	//returns status of Inputpin # n if SWITCH, return -1
+
+	virtual void setInputPinStatus(int n, STATUS s);	//set status of Inputpin # n, to be used by connection class.
+
+};
