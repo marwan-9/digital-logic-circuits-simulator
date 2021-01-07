d973d6995082dd26d9cbdf7fa858a8b0c0b238e4
YoussofEldeeb <76092209+YoussofEldeeb@users.noreply.github.com>
2021-01-07 23:04:50 +0200

    Update Component.h

--- Project Code Framework/Components/Component.h
+++ Project Code Framework/Components/Component.h
@@ -11,18 +11,22 @@ private:
 	string m_Label;
 protected:
 	GraphicsInfo m_GfxInfo;	//The parameters required to draw a component
+	bool IfSelected; //if component is selected or not
 public:
 	Component(const GraphicsInfo &r_GfxInfo);
 	virtual void Operate() = 0;	//Calculates the output according to the inputs
 	virtual void Draw(Output* pOut) = 0;	//for each component to Draw itself
 	
-	
+	virtual bool Inside(int x, int y);
+
 	virtual int GetOutPinStatus()=0;	//returns status of outputpin if LED, return -1
 	virtual int GetInputPinStatus(int n)=0;	//returns status of Inputpin # n if SWITCH, return -1
 
 	virtual void setInputPinStatus(int n, STATUS s)=0;	//set status of Inputpin # n, to be used by connection class.
 
-	
+	void SetIfSelected(bool q);
+	bool GetIfSelected();
+
 	Component();	
 	
 	//Destructor must be virtual
