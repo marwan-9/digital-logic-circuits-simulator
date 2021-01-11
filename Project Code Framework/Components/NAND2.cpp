841379dd5989b42325dab2c03c8ffcfacb10acee
YoussofEldeeb <76092209+YoussofEldeeb@users.noreply.github.com>
2021-01-11 19:37:48 +0200

    Update NAND2.cpp

--- Project Code Framework/Components/NAND2.cpp
+++ Project Code Framework/Components/NAND2.cpp
@@ -22,7 +22,7 @@ void NAND2::Operate()
 void NAND2::Draw(Output* pOut)
 {
 	//Call output class and pass gate drawing info to it.
-	pOut->DrawNAND2(m_GfxInfo);
+	pOut->DrawNAND2(m_GfxInfo, IfSelected);
 }
 
 //returns status of outputpin
