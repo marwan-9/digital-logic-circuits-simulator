9cc7d1538d5df0684bde01308002b79b61e7d192
ahmed1234552 <74154389+ahmed1234552@users.noreply.github.com>
2021-01-11 21:52:45 +0200

    update label

--- Project Code Framework/Components/Gate.h
+++ Project Code Framework/Components/Gate.h
@@ -1,3 +1,35 @@
+<<<<<<< HEAD
+#ifndef _GATE_H
+#define _GATE_H
+
+/*
+  Class Gate
+  -----------
+  - Base class for all types of gates
+  - Each gate has n inputs pins and one output pin
+*/
+
+
+#include "InputPin.h"
+#include "OutputPin.h"
+#include "Component.h"
+
+class Gate:public Component
+{
+protected:
+	InputPin* m_InputPins;	//Array of input pins of the Gate
+	OutputPin m_OutputPin;	//The Gate output pin
+	int m_Inputs;		//No. of input pins of that Gate.
+public:
+	Gate(int r_Inputs, int r_FanOut);
+	virtual void DrawFrame(Output* pOut);
+	virtual Component* Copy()=0;
+	virtual int GetNumOfInputs();
+	//void drawname(Output* pOut, int Cx, int Cy, const char* cpText); //virtual
+};
+
+#endif
+=======
 #ifndef _GATE_H
 #define _GATE_H
 
@@ -33,3 +65,4 @@ public:
 };
 
 #endif
+>>>>>>> 1acd831287fc1c46a557598d0bda24f5b1254083
